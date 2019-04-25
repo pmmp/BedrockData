8c84269e485de6fc35d42dda7f9d448740cccd17
Dylan K. Taylor <odigiman@gmail.com>
2019-04-25 17:39:15 +0100

    added a script to minify JSON for production builds

---
+++ minify_json.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+$ext = ".json";
+$extLen = strlen($ext);
+foreach(scandir(__DIR__, SCANDIR_SORT_NONE) as $file){
+	if(substr($file, -$extLen) === $ext){
+		file_put_contents($file, json_encode(json_decode(file_get_contents($file))));
+		echo "Minified JSON file $file\n";
+	}
+}
